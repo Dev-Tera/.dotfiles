bbb3ed1cc239eaa463ee373c1e2699efaf081dd2
benedikt <benedikt.wutzke@gmail.com>
2026-07-29 21:03:54 +0200

    Add keybinds for writing and quiting neovim

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -37,3 +37,6 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Window left" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Window down" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Window up" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Speichern' })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Speichern & beenden' })
